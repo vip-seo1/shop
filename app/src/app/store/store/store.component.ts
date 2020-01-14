2df48a26ff548c2b5c2d39eb77cc4e68e0b7cb24
vip-seo <csat.vip@gmail.com>
2020-01-14 09:28:17 +0200

    jj

--- app/src/app/store/store/store.component.ts
+++ app/src/app/store/store/store.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { Product, DataSourceService} from '../../model/data-source.service';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  Product,
+  DataSourceService
+} from '../../model/data-source.service';
 @Component({
   selector: 'app-store',
   templateUrl: './store.component.html',
@@ -7,19 +13,29 @@ import { Product, DataSourceService} from '../../model/data-source.service';
 })
 export class StoreComponent implements OnInit {
   productList: Product[] = [];
-  categories: Set<string> | string[] = [];
+  categories: Set < string > | string[] = [];
   currentCategory: null;
 
-  constructor(private ds: DataSourceService) { }
+  constructor(private ds: DataSourceService) {}
 
   ngOnInit() {
-    
+      if (this.currentCategory == null) {
     this.ds.getProduct().subscribe((products: Product[]) => {
       this.productList = products;
       const temp = products.map((p) => p.category);
       this.categories = new Set(temp);
-   });
+    });
+  } else {this.getProduct();}
+    }
 
-   
-  }
+     getCategory(n:any){
+       this.currentCategory = n;
+     }
+     
+   getProduct(): Product[] {
+     
+        return this.productList.filter((product) => product.category === this.currentCategory);
+      
+    }
 }
+
