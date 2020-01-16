0a46ba7ca253026cfb2dd0af3f6bff33ce9642bc
vip-seo <csat.vip@gmail.com>
2020-01-16 19:50:44 +0200

    some

--- app/src/app/admin/admin.module.ts
+++ app/src/app/admin/admin.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { ProductsComponent } from './products/products.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent, ProductsComponent],
   imports: [
     CommonModule
   ]
